9ed7c890d53c1caa3e84f41851b76a4b97e2222a
outlierlo <allenlo0130@gmail.com>
2022-05-18 21:09:36 +0800

    [mod] button layout, adjust border radius and border width

--- lib/util/layout.dart
+++ lib/util/layout.dart
@@ -2,4 +2,6 @@ class Layout {
   static double commonPadding = 10.0;
   static double mainAxisSpacing = 10.0;
   static double crossAxisSpacing = 10.0;
+  static double commonBorderWidth = 3.0;
+  static double commonBorderRadius = 10.0;
 }
--- lib/widget/button.dart
+++ lib/widget/button.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:peerrev/util/layout.dart';
+
 class Button extends StatelessWidget {
   final Color color;
   final IconData? icon;
@@ -32,18 +34,32 @@ class Button extends StatelessWidget {
           ? ButtonStyle(
               backgroundColor: MaterialStateProperty.all<Color>(color),
               foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
-              side: MaterialStateProperty.all<BorderSide>(BorderSide(
-                width: 3,
-                color: color,
-              )),
+              side: MaterialStateProperty.all<BorderSide>(
+                BorderSide(
+                  width: Layout.commonBorderWidth,
+                  color: color,
+                ),
+              ),
+              shape: MaterialStateProperty.all<OutlinedBorder>(
+                RoundedRectangleBorder(
+                  borderRadius:
+                      BorderRadius.circular(Layout.commonBorderRadius),
+                ),
+              ),
             )
           : ButtonStyle(
               backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
               foregroundColor: MaterialStateProperty.all<Color>(color),
               side: MaterialStateProperty.all<BorderSide>(BorderSide(
-                width: 3,
+                width: Layout.commonBorderWidth,
                 color: color,
               )),
+              shape: MaterialStateProperty.all<OutlinedBorder>(
+                RoundedRectangleBorder(
+                  borderRadius:
+                      BorderRadius.circular(Layout.commonBorderRadius),
+                ),
+              ),
             ),
     );
   }
