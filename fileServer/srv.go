973a86a45eaf9a22f4307dc4f85584f65d07cf86
outlierlo <allenlo0130@gmail.com>
2022-05-20 10:04:44 +0800

    [mod] gofmt

--- fileServer/srv.go
+++ fileServer/srv.go
@@ -1,38 +1,38 @@
 package main
 
 import (
-    "log"
-    "net/http"
+	"log"
+	"net/http"
 	"strings"
 )
 
 type securedFileSystem struct {
-    fs http.FileSystem
+	fs http.FileSystem
 }
 
 func (sfs securedFileSystem) Open(path string) (http.File, error) {
-    f, err := sfs.fs.Open(path)
-    if err != nil {
-        return nil, err
-    }
+	f, err := sfs.fs.Open(path)
+	if err != nil {
+		return nil, err
+	}
 
-    s, err := f.Stat()
-    if s.IsDir() {
-        index := strings.TrimSuffix(path, "/") + "/index.html"
-        if _, err := sfs.fs.Open(index); err != nil {
-            return nil, err
-        }
-    }
+	s, err := f.Stat()
+	if s.IsDir() {
+		index := strings.TrimSuffix(path, "/") + "/index.html"
+		if _, err := sfs.fs.Open(index); err != nil {
+			return nil, err
+		}
+	}
 
-    return f, nil
+	return f, nil
 }
 
 func main() {
 	fs := http.FileServer(securedFileSystem{http.Dir("build/")})
-    http.Handle("/", http.StripPrefix("/", fs))
-    http.Handle("/assets/", http.StripPrefix("/assets/", fs))
-    err := http.ListenAndServe(":57230", nil)
-    if err != nil {
-        log.Fatal("ListenAndServe: ", err)
-    }
+	http.Handle("/", http.StripPrefix("/", fs))
+	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
+	err := http.ListenAndServe(":57230", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
