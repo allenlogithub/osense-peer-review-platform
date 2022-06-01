91cd93e155855d89edb07ebaea640f5d9b7e3c2d
outlierlo <allenlo0130@gmail.com>
2022-06-01 22:39:47 +0800

    [mod] ReviewList, remake

--- lib/widget/staff/reviewedList.dart
+++ lib/widget/staff/reviewedList.dart
@@ -4,108 +4,157 @@ import 'package:peerrev/data/repo/reviewedScore.dart';
 import 'package:peerrev/util/style.dart' as style;
 import 'package:peerrev/widget/tag.dart';
 import 'package:peerrev/widget/scoreIndicator.dart';
-import 'package:peerrev/util/layout.dart';
+import 'package:peerrev/util/layout.dart' as layout;
 import 'package:peerrev/widget/roundedBox.dart';
 
 class ReviewList extends StatelessWidget {
   ReviewList({Key? key}) : super(key: key);
 
   var data = ReviewedScore.data;
+  final List<TableRow> _tableRows = [];
+  final headRowSpacer = _rowSpacer(spaceUnit: 2);
+  final rowSpacer = _rowSpacer();
 
   @override
   Widget build(BuildContext context) {
-    print(MediaQuery.of(context).size.width);
-    print(MediaQuery.of(context).size.height);
-    return RoundedBox(
-      toTop: 70.0,
-      toLeft: 32.0,
-      toRight: 180.0,
-      toBottom: 32.0,
-      color: Colors.grey.shade200,
-      child: Column(
+    _tableRows.add(
+      TableRow(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(
+              Radius.circular(layout.Layout.commonBorderRadius)),
+          border: Border.all(
+            color: Colors.black,
+            width: 1,
+          ),
+          color: Colors.white,
+        ),
         children: [
-          RoundedBox(
-            toTop: 70.0,
-            toLeft: 32.0,
-            toRight: 32.0,
-            child: Container(
-              height: 56.0,
-              child: Row(
-                children: [
-                  // have no idea in adjusting the spaces cleverly
-                  const SizedBox(width: 116.0),
-                  Text('姓名', style: style.Text.boldH4),
-                  const SizedBox(width: 80.0),
-                  Text('部門', style: style.Text.boldH4),
-                  const SizedBox(width: 80.0),
-                  Expanded(
-                      child: Center(
-                    child: Text('評分', style: style.Text.boldH4),
-                  )),
-                  const SizedBox(width: 80.0),
-                  Text('功能', style: style.Text.boldH4),
-                  const SizedBox(width: 32),
-                ],
-              ),
-            ),
-          ),
-          Expanded(
-            child: Center(
-              child: ListView.builder(
-                padding: const EdgeInsets.all(8.0),
-                itemCount: data.length,
-                itemBuilder: (BuildContext context, int index) {
-                  String name = data[index]['name'].toString();
-                  String dept = data[index]['dept'].toString();
-                  List<String> score = data[index]['score'] as List<String>;
-
-                  return Container(
-                    height: 120,
-                    // width: 300,
-                    width: MediaQuery.of(context).size.width, // - 425,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const SizedBox(width: 116.0),
-                        Text(name, style: style.Text.normH4),
-                        const SizedBox(width: 80.0),
-                        Tag(color: _deptColorMap.CMap[dept]!, text: dept),
-                        // const SizedBox(width: 80.0),
-                        Spacer(),
-                        Expanded(
-                          child: Center(
-                            child: ListView.builder(
-                              scrollDirection: Axis.horizontal,
-                              itemCount: score.length,
-                              itemBuilder: (BuildContext context, int idx) {
-                                return ScoreIndicator(
-                                  color: _colorList.colorList[idx],
-                                  bgColor: _colorList.bgColorList[idx],
-                                  radius: 34.0,
-                                  width: 8.5,
-                                  value: double.parse(score[idx]),
-                                  maximum: 5,
-                                );
-                              },
-                            ),
-                          ),
-                        ),
-                        Spacer(),
-                        // const SizedBox(width: 80.0),
-                        const _iconBtn(icon: Icons.edit, callback: _edit),
-                        const SizedBox(width: 18.0),
-                        const _iconBtn(icon: Icons.delete, callback: _delete),
-                        const SizedBox(width: 32.0),
-                      ],
-                    ),
-                  );
-                },
-              ),
-            ),
-          ),
+          SizedBox(
+              height: layout.Size.blockSizeVertical * 5,
+              child: Center(
+                  child: Text('姓名',
+                      style: style.Text.boldH4, textAlign: TextAlign.center))),
+          SizedBox(
+              height: layout.Size.blockSizeVertical * 5,
+              child: Center(
+                  child: Text('部門',
+                      style: style.Text.boldH4, textAlign: TextAlign.center))),
+          SizedBox(
+              height: layout.Size.blockSizeVertical * 5,
+              child: Center(
+                  child: Text('評分',
+                      style: style.Text.boldH4, textAlign: TextAlign.center))),
+          SizedBox(
+              height: layout.Size.blockSizeVertical * 5,
+              child: Center(
+                  child: Text('功能',
+                      style: style.Text.boldH4, textAlign: TextAlign.center))),
         ],
       ),
     );
+    _tableRows.add(headRowSpacer);
+
+    for (int i = 0; i < data.length; i++) {
+      String name = data[i]['name'].toString();
+      String dept = data[i]['dept'].toString();
+      List<String> score = data[i]['score'] as List<String>;
+      List<Widget> _socreIndicators = [];
+
+      for (int j = 0; j < score.length; j++) {
+        _socreIndicators.add(
+          ScoreIndicator(
+            color: _colorList.colorList[j],
+            bgColor: _colorList.bgColorList[j],
+            radius: layout.Size.blockSize * 3,
+            width: layout.Size.blockSize,
+            value: double.parse(score[j]),
+            maximum: 5,
+          ),
+        );
+        if (j != score.length - 1) {
+          _socreIndicators
+              .add(SizedBox(width: layout.Size.blockSizeHorizontal * 1));
+        }
+      }
+      _tableRows.add(
+        TableRow(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(
+                Radius.circular(layout.Layout.commonBorderRadius)),
+            border: Border.all(
+              color: Colors.black,
+              width: 1,
+            ),
+            color: Colors.white,
+          ),
+          children: [
+            SizedBox(
+              child: Center(
+                  child: Text(name,
+                      style: style.Text.normH4, textAlign: TextAlign.center)),
+              height: layout.Size.blockSizeVertical * 10,
+            ),
+            SizedBox(
+              child: Center(
+                  child: Center(
+                      child:
+                          Tag(color: _deptColorMap.CMap[dept]!, text: dept))),
+              height: layout.Size.blockSizeVertical * 10,
+            ),
+            SizedBox(
+              child: Center(
+                  child: Center(
+                      child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: _socreIndicators,
+              ))),
+              height: layout.Size.blockSizeVertical * 10,
+            ),
+            SizedBox(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  _iconBtn(icon: Icons.edit, callback: _edit),
+                  _iconBtn(icon: Icons.delete, callback: _delete),
+                ],
+              ),
+              height: layout.Size.blockSizeVertical * 10,
+            ),
+          ],
+        ),
+      );
+      if (i != score.length - 1) {
+        _tableRows.add(rowSpacer);
+      }
+    }
+
+    return RoundedBox(
+        toTop: layout.Size.blockSizeVertical * 7,
+        toLeft: layout.Size.blockSizeHorizontal * 3,
+        toRight: layout.Size.blockSizeHorizontal * 12,
+        toBottom: layout.Size.blockSizeVertical * 3,
+        color: Colors.grey.shade200,
+        child: Padding(
+          padding: EdgeInsets.only(
+            top: layout.Size.blockSizeVertical * 6,
+            bottom: layout.Size.blockSizeVertical,
+            left: layout.Size.blockSizeHorizontal * 2,
+            right: layout.Size.blockSizeHorizontal * 2,
+          ),
+          child: Column(
+            children: [
+              Table(
+                children: _tableRows,
+                columnWidths: const {
+                  0: FlexColumnWidth(3),
+                  1: FlexColumnWidth(3),
+                  2: FlexColumnWidth(9),
+                  3: FlexColumnWidth(2),
+                },
+              ),
+            ],
+          ),
+        ));
   }
 }
 
@@ -132,7 +181,7 @@ class _iconBtn extends StatelessWidget {
       onPressed: callback,
       icon: Icon(icon),
       color: Colors.grey.shade500,
-      iconSize: 24.0,
+      iconSize: layout.Size.blockSize * 2,
     );
   }
 }
@@ -164,4 +213,36 @@ class _colorList {
     const Color.fromRGBO(125, 193, 220, 0.25),
     const Color.fromRGBO(136, 136, 136, 0.25),
   ];
+}
+
+final rowSpacer = TableRow(children: [
+  SizedBox(
+    height: layout.Size.blockSizeVertical,
+  ),
+  SizedBox(
+    height: layout.Size.blockSizeVertical,
+  ),
+  SizedBox(
+    height: layout.Size.blockSizeVertical,
+  ),
+  SizedBox(
+    height: layout.Size.blockSizeVertical,
+  ),
+]);
+
+TableRow _rowSpacer({spaceUnit = 1}) {
+  return TableRow(children: [
+    SizedBox(
+      height: layout.Size.blockSizeVertical * spaceUnit,
+    ),
+    SizedBox(
+      height: layout.Size.blockSizeVertical * spaceUnit,
+    ),
+    SizedBox(
+      height: layout.Size.blockSizeVertical * spaceUnit,
+    ),
+    SizedBox(
+      height: layout.Size.blockSizeVertical * spaceUnit,
+    ),
+  ]);
 }
